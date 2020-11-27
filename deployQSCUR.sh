fba9cf01a6f2c95195a83e1999bb198c9230e638
adamhucn <44492031+adamhucn@users.noreply.github.com>
2020-11-27 21:39:06 +0800

    Update deployQSCUR.sh

--- deployQSCUR.sh
+++ deployQSCUR.sh
@@ -1,6 +1,6 @@
 ### Powered by AWS Enterprise Support 
 ### Mail: tam-solution-costvisualization@amazon.com
-### Version 1.21.2
+### Version 1.21.3
 
 checkJQ() {
 	# Check if jq is an executable command
